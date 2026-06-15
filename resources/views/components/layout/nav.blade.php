66b41321683d2a863f72d6229d3256c7dee0572e
hws <mxm0028@gmail.com>
2026-06-15 14:11:54 +0300

    setup project and theme prep, stopped at creating register form

---
+++ resources/views/components/layout/nav.blade.php
@@ -0,0 +1,15 @@
+<nav class="border-b border-border px-6">
+    <div class="max-w-7xl mx-auto h-16 flex items-center justify-between">
+        <div>
+            <a href="">
+                LOGO
+            </a>
+        </div>
+    
+        <div class="flex gap-x-5 items-center">
+            <a href="">Sign In</a>
+            <a href="" class="btn">Register</a>
+        </div>
+
+    </div>
+</nav>
